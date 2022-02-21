eb041f503ad660556b420e0793b64ee60042b99c
maxib0n <99250518+maxib0n@users.noreply.github.com>
2022-02-21 11:38:29 +0100

    roba html

--- EX/wwwroot/Scripts/Site.js
+++ EX/wwwroot/Scripts/Site.js
@@ -1,1 +1,19 @@
 ﻿
+TestApi() {
+    $.ajax({
+        url: "Api/Course",
+        method: "GET"
+    }).done(function (result) {
+        if (result)
+            $("#contenuto").html(result.map(item => `<tr>
+                <td>${item.id} </td>
+                <td>${item.title}</td>
+                <td>${item.duration}</td>
+                <td>${new Date(item.startDate).toLocaleDateString()}</td>
+                <td>${new Date(item.endDate).toLocaleDateString()}</td>
+              </tr>`));
+    }).fail(function () {
+        alert("ERRORE!");
+    }).always(function () {
+        $("#esito").html("Tasto schiacciato");
+    })
