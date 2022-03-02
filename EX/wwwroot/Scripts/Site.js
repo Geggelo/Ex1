2732f3eab2b6cb7f5081391f27f2a13b7e1830a8
Geggelo <gabbatore.angelo@gmail.com>
2022-03-02 12:07:43 +0100

    Dovrebbe essere finito

--- EX/wwwroot/Scripts/Site.js
+++ EX/wwwroot/Scripts/Site.js
@@ -18,7 +18,7 @@ function GetCities() {
                 <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
                 </svg></button>
 
-                <button type="button" class="btn btn-success" onclick="CityPopUp()">
+                <button type="button" class="btn btn-success" onclick="PeoplePopUp('${item.id}')">
                 <i class="bi bi-plus-circle"></i>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
@@ -205,13 +205,14 @@ function CreatePersonFromCity(id) {
 }
 
 //metodo per fare apparire il pop up con il form
-function PeoplePopUp() {
-
+function PeoplePopUp(id) {
+    document.getElementById('peopleCreator').style.display = "block";
+    $("#cityId").val(id);
 }
 
-//metodo per fare apparire il pop up con il form
-function CityPopUp() {
-
+//Function To Display Popup
+function PeoplePopUpHide() {
+    document.getElementById('peopleCreator').style.display = "none";
 }
 
 //per ora è inutile
@@ -219,5 +220,3 @@ function ClearForms() {
     $("form").removeClass('was-validated');
     $(".form-control").val("");
 }
-
-
