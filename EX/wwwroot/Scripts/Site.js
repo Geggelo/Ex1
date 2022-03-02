c3af85e593cdec0b84bb49e65932e536d8c1d232
Geggelo <gabbatore.angelo@gmail.com>
2022-03-02 09:23:51 +0100

    Sistemato metodo per creazione

--- EX/wwwroot/Scripts/Site.js
+++ EX/wwwroot/Scripts/Site.js
@@ -18,11 +18,18 @@ function GetCities() {
                 <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
                 </svg></button>
 
+                <button type="button" class="btn btn-success" onclick="CityPopUp()">
+                <i class="bi bi-plus-circle"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                </svg></button>
+
                 <button type="button" class="btn btn-danger" onclick="DeleteCity('${item.id}')"
                 <i class="bi bi-trash3"></i>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                 <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
                 </svg></button>
+                
                 </td>
                 </tr>`));
                 $("#cities").show();
@@ -147,6 +154,7 @@ function Return() {
 }
 
 //Fa correttamente la post, dobbiamo metterci la validation ed è a posto
+//bisogna aggiornare con il nuovo form
 function DioCaroSeNonFunzioni() {
     var person = {
         id: "00000000-0000-0000-0000-000000000000",
@@ -154,7 +162,7 @@ function DioCaroSeNonFunzioni() {
         lastName: $("#lastName").val(),
         age: $("#age").val(),
         address: $("#address").val(),
-        cityId: "bf79ef86-53eb-48f8-aa22-3c84e0cda400"
+        cityId: "" //bisogna settarlo dopo aver creato il menu a tendina
     };
 
     $.ajax({
@@ -169,6 +177,41 @@ function DioCaroSeNonFunzioni() {
     }).always(function () {
         alert("Chiamato");
     });
+}
+
+//Metodo per creare una persona dalla lista di città, bisogna aggiornare con il nuovo form
+function CreatePersonFromCity(id) {
+    var person = {
+        id: "00000000-0000-0000-0000-000000000000",
+        firstName: $("#firstName").val(),
+        lastName: $("#lastName").val(),
+        age: $("#age").val(),
+        address: $("#address").val(),
+        cityId: `${id}`
+    };
+
+    $.ajax({
+        method: "POST",
+        url: "https://localhost:7137/Person",
+        data: JSON.stringify(person),
+        contentType: "application/json"
+    }).done(function (result) {
+        Return();
+    }).fail(function () {
+        alert("Fallito");
+    }).always(function () {
+        alert("Chiamato");
+    });
+}
+
+//metodo per fare apparire il pop up con il form
+function PeoplePopUp() {
+
+}
+
+//metodo per fare apparire il pop up con il form
+function CityPopUp() {
+
 }
 
 //per ora è inutile
